f03f1fce43cee8e3064a286ccdcb8437e906422c
mental1104 <mental1104@gmail.com>
2024-05-11 23:41:22 +0800

    引入性格倍率枚举

--- api/common/damage_calculator.py
+++ api/common/damage_calculator.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator 
 from typing import Optional
 from enum import Enum
 import sys
@@ -16,18 +16,23 @@ class CommonProperty(BaseModel):
     special_defense: int = 0
     speed: int = 0
 
-
+# 个体值
 class IndividualValues(CommonProperty):
     pass
 
+# 种族值
 class SpeciesStrength(CommonProperty):
     pass
 
+# 努力值
 class BasePoints(CommonProperty):
     pass
 
+# 最终能力值
 class Statistic(CommonProperty):
     pass
+
+# 性格增益
 
 
 class MoveType:
@@ -41,8 +46,49 @@ class Move:
     move_type: MoveType
 
 
-class PokemonEntity:
+class PropertyCalculator:
+    @staticmethod
+    def calculate_hp(level: int, species_strength: int, basepoint: int = 252, individual_values: int = 31):
+        return ((species_strength * 2 + individual_values + basepoint / 4) * level) / 100.0 + 10 + level
+
+    @staticmethod
+    def calculate_ability(level: int, species_strength: int, basepoint: int = 252, individual_values: int = 31, nature: str = ""):
+        return (((species_strength * 2 + individual_values + basepoint / 4) * level) / 100.0 + 5) * 1.0
+
+
+class Nature(str, Enum):
+    HARDY = 'Hardy'  # 勤奋
+    LONELY = 'Lonely' # 怕寂寞
+    ADAMANT = 'Adamant' # 固执
+    NAUGHTY = 'Naughty' # 顽皮
+    BRAVE = 'Brave' # 勇敢
+    BOLD = 'Bold' # 大胆
+    DOCILE = 'Docile' # 坦率
+    IMPISH = 'Impish' # 淘气
+    LAX = 'Lax' # 乐天
+    RELAXED = 'Relaxed' # 悠闲
+    MODEST = 'Modest' # 内敛
+    MILD = 'Mild' # 慢吞吞
+    BASHFUL = 'Bashful' # 害羞
+    RASH = 'Rash' # 马虎
+    QUIET = 'Quiet' # 冷静
+    CALM = 'Calm' # 温和
+    GENTLE = 'Gentle' # 温顺
+    CAREFUL = 'Careful' # 慎重
+    QUIRKY = 'Quirky' # 浮躁
+    SASSY = 'Sassy' # 自大
+    TIMID = 'Timid' # 胆小
+    HASTY = 'Hasty' # 急躁
+    JOLLY = 'Jolly' # 爽朗
+    NAIVE = 'Naive' # 天真
+    SERIOUS = 'Serious' # 认真
     
+nature_dict = {
+    "Timid": {"attack": 0.9, "defense": 1.0, "special_attack": 1.0, "special_defense": 1.0, "speed": 1.1},
+}
+
+
+class PokemonEntity:
     def __init__(self, 
         id, 
         level: int,
@@ -55,12 +101,9 @@ class PokemonEntity:
         self.name = ""
         self.level = level
         species_strength = SpeciesStrength()
-        print(111)
+
         with open_session() as session:
-            print(123232)
             pokemon_record = Pokemon.get_by_id(session, id)
-            print(555322)
-            print(pokemon_record)
             if pokemon_record is not None:
                 species_strength = SpeciesStrength(
                     hp=pokemon_record.hp,
@@ -71,37 +114,21 @@ class PokemonEntity:
                     speed=pokemon_record.speed
                 )
                 self.name = pokemon_record.name
-                print(self.name)
+
         self.species_strength = species_strength
         self.basepoint = basepoint
         self.individual_values = individual_values
+        self.stat = Statistic()
+        self.refresh()
+    
+    def refresh(self):
+        self.stat.hp = PropertyCalculator.calculate_hp(self.level, self.species_strength.hp, self.basepoint.hp, self.individual_values.hp)
+        self.attack = PropertyCalculator.calculate_ability(self.level, self.species_strength.attack, self.basepoint.attack, self.individual_values.attack)
+        self.defense = PropertyCalculator.calculate_ability(self.level, self.species_strength.defense, self.basepoint.defense, self.individual_values.defense)
+        self.special_attack = PropertyCalculator.calculate_ability(self.level, self.species_strength.special_attack, self.basepoint.special_attack, self.individual_values.special_attack)
+        self.special_defense = PropertyCalculator.calculate_ability(self.level, self.species_strength.special_defense, self.basepoint.special_defense, self.individual_values.special_defense)
+        self.speed = PropertyCalculator.calculate_ability(self.level, self.species_strength.speed, self.basepoint.speed, self.individual_values.speed)
 
-class PropertyCalculator:
-    @staticmethod
-    def calculate_hp(level: int, species_strength: int, basepoint: int = 252, individual_values: int = 31):
-        print(level)
-        print(species_strength)
-        print(basepoint)
-        print(individual_values)
-        return ((species_strength * 2 + individual_values + basepoint / 4) * level) / 100.0 + 10 + level
-
-    @staticmethod
-    def calculate_ability(level: int, species_strength: int, basepoint: int = 252, individual_values: int = 31, nature: str = ""):
-        return (((species_strength * 2 + individual_values + basepoint / 4) * level) / 100.0 + 5) * 1.0
-        
-    def __init__(self, pokemon_entity: PokemonEntity):
-        stat = Statistic()
-        species_strength = pokemon_entity.species_strength
-        basepoint = pokemon_entity.basepoint
-        individual_values = pokemon_entity.individual_values
-        stat.hp = PropertyCalculator.calculate_hp(pokemon_entity.level, species_strength.hp, basepoint.hp, individual_values.hp)
-        stat.attack = PropertyCalculator.calculate_ability(pokemon_entity.level, species_strength.attack, basepoint.attack, individual_values.attack)
-        stat.defense = PropertyCalculator.calculate_ability(pokemon_entity.level, species_strength.defense, basepoint.defense, individual_values.defense)
-        stat.special_attack = PropertyCalculator.calculate_ability(pokemon_entity.level, species_strength.special_attack, basepoint.special_attack, individual_values.special_attack)
-        stat.special_defense = PropertyCalculator.calculate_ability(pokemon_entity.level, species_strength.special_defense, basepoint.special_defense, individual_values.special_defense)
-        stat.speed = PropertyCalculator.calculate_ability(pokemon_entity.level, species_strength.speed, basepoint.speed, individual_values.speed)
-        self.stat = stat
-        
 
 class DamageCalculator:
     
