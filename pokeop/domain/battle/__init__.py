7f4ec995d3999d37e280a6c889e9081cd6bb544c
Blue Espeon <mental1104@gmail.com>
2026-07-23 23:32:54 +0800

    chore: export battle transition primitives

--- pokeop/domain/battle/__init__.py
+++ pokeop/domain/battle/__init__.py
@@ -14,6 +14,19 @@ from pokeop.domain.battle.items import DamageItem
 from pokeop.domain.battle.modifier_keys import ModifierKey
 from pokeop.domain.battle.side_conditions import SideConditions
 from pokeop.domain.battle.terrain import Terrain
+from pokeop.domain.battle.transitions import (
+    StateKeyed,
+    TransitionEvent,
+    TransitionEventSummary,
+    TransitionEventType,
+    WeightedTransition,
+    branch_transitions,
+    combine_independent_transitions,
+    damage_rolls_to_transitions,
+    merge_equivalent_transitions,
+    normalize_transition_weights,
+    validate_transition_distribution,
+)
 from pokeop.domain.battle.weather import Weather
 
 
@@ -29,7 +42,18 @@ __all__ = [
     "ModifierKey",
     "MoveCategory",
     "SideConditions",
+    "StateKeyed",
     "Terrain",
+    "TransitionEvent",
+    "TransitionEventSummary",
+    "TransitionEventType",
     "Weather",
+    "WeightedTransition",
+    "branch_transitions",
+    "combine_independent_transitions",
+    "damage_rolls_to_transitions",
     "is_grounded",
+    "merge_equivalent_transitions",
+    "normalize_transition_weights",
+    "validate_transition_distribution",
 ]
